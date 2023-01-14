9ac42dae133229185e668e7d27e0bd8f10629549
AlienCat1 <ram.slsgoc@gmail.com>
2023-01-14 11:55:17 +0530

    only styling pending

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import Login from './majorComponents/Login'
+import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import LoginError from './majorComponents/LoginError'
 // import Footer from './components/Footer'
 import Header from './components/Header'
 import Blog from './majorComponents/Blog'
@@ -11,42 +9,69 @@ import Error from './majorComponents/Error'
 import Home from './majorComponents/Home'
 import Tutorial from './majorComponents/Tutorial'
 import useForceUpdate from 'use-force-update'
+import Users from './user.json'
+import { useState } from 'react'
 
 const App = () => {
 
   const forceUpdate = useForceUpdate()
 
-  const[loggedinMode, setLoggedinMode]=useState(false)
+  const[username, setUsername] = useState('')
+  const[password, setPassword] = useState('')
+  const[displayType, setDisplayType] = useState(true)
+
+  const handleLogin = () => {
+    const auth = Users.filter(curElem=>curElem.username===username)
+      if (auth.length!==0) {
+        if (auth[0].password===password) {
+          localStorage.setItem('loggedin', username)
+          setUsername('')
+          setPassword('')
+        }else{
+          alert('Invalid Password!')
+        }
+      }else{
+          alert('No User Found!')
+        }
+  }
 
   const handleLogout=()=>{
     localStorage.removeItem('loggedin')
   }
 
-  useEffect(()=>{
-    var user = localStorage.getItem('loggedin')
+  var user = localStorage.getItem('loggedin')
     if (user===null || user==='') {
-      setLoggedinMode(false)
+      return(
+        <>
+         <h2>LOGIN</h2>
+         <hr/>
+         <input type='text' placeholder='Username' value={username} onChange={e=>setUsername(e.target.value)} /> <br/>
+
+         <input type={displayType? 'password':'text'} placeholder='Password' value={password} onChange={e=>setPassword(e.target.value)}/>
+         <button onClick={()=>{setDisplayType(!displayType)}}>{displayType? 'Show':'Hide'}</button> <br/>
+
+         <button onClick={()=>{handleLogin(); forceUpdate()}}>LOGIN</button>
+        </>
+      )
     } else {
-      setLoggedinMode(true)
+      return(
+        <BrowserRouter>
+          <div>
+           <Header />
+           <h3>Welcome {localStorage.getItem('loggedin')}</h3>
+           <button onClick={()=>{handleLogout(); forceUpdate()}}>LOGOUT</button>
+          </div>
+          <Routes>
+           <Route path='/' element={<Home/>} />
+           <Route path='/blog' element={<Blog/>} />
+           <Route path='/community' element={<Community/>} />
+           <Route path='/docs' element={<Docs/>} />
+           <Route path='/*' element={<Error/>} />
+           <Route path='/tutorial' element={<Tutorial/>} />
+          </Routes>
+        </BrowserRouter>
+      )
     }
-  },[])
-  
-  return (    
-    <BrowserRouter>
-     {loggedinMode?
-     <div>
-     <Header />
-     <h3>Welcome {localStorage.getItem('loggedin')}</h3><button onClick={handleLogout}>LOGOUT</button></div>:''}
-        <Routes>
-          <Route exact path='/' element={(loggedinMode?(<Home/>):(<Login/>))} />
-          <Route exact path='/blog' element={(loggedinMode?(<Blog/>):(<LoginError/>))} />
-          <Route exact path='/community' element={(loggedinMode?(<Community/>):(<LoginError/>))} />
-          <Route exact path='/docs' element={(loggedinMode?(<Docs/>):(<LoginError/>))} />
-          <Route exact path='/*' element={(loggedinMode?(<Error/>):(<LoginError/>))} />
-          <Route exact path='/tutorial' element={(loggedinMode?(<Tutorial/>):(<LoginError/>))} />
-        </Routes>
-    </BrowserRouter>
-  )
 }
 
 export default App
